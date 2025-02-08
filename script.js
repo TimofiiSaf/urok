f79e04009c75a5c90bab6331e5d5f1b115004c38
Timofii <timiqxx29@gmail.com>
2025-02-08 11:06:16 +0200

    g

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-const myName='13'
+const title = document.querySelector('.title')
+console.log(title);
+title.textContent = 'Good bye'
+const my = document.querySelector('p')
+console.log(my)
+my.style.color = 'green'
+const btn = document.querySelector('#btn')
+console.log(btn)
+const password = document.querySelector('input[type="password"]')
+console.log(password);
+const colors = document.querySelectorAll('.color')
+console.log(colors)
 
